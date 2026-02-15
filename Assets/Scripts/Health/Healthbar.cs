89db3e39cefa9fb7e9a92fea387893e0e56277f0
XAchtung <krisbery@o2.pl>
2026-02-15 23:58:56 +0100

    Added Health mechanics
    Character's health + healing + dealing damage (saw)

---
+++ Assets/Scripts/Health/Healthbar.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Healthbar : MonoBehaviour
+{
+    [SerializeField] private Health playerHealth;
+    [SerializeField] private UnityEngine.UI.Image totalhealthBar;
+    [SerializeField] private UnityEngine.UI.Image currenthealthBar;
+
+    private void Start()
+    {
+        totalhealthBar.fillAmount = playerHealth.currentHealth / 10;
+    }
+
+    private void Update()
+    {
+        currenthealthBar.fillAmount = playerHealth.currentHealth / 10;
+    }
+}
